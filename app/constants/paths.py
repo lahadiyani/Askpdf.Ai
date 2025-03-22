3658643c8d1f3c6755dee8aa71e8cfaeccb5e5cd
Hadiani <80145828+lahadiyani@users.noreply.github.com>
2025-03-23 06:38:28 +0800

    Update paths.py

--- app/constants/paths.py
+++ app/constants/paths.py
@@ -1,6 +1,6 @@
 import os
 
 PATH_PROGRAM = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-UPLOAD_FOLDER = f"/tmp/{PATH_PROGRAM}-storage"
+UPLOAD_FOLDER = f"/tmp/{PATH_PROGRAM}"
 HISTORY_FILE = f"/tmp/{PATH_PROGRAM}-history.json"
 ALLOWED_EXTENSIONS = ["pdf"]
