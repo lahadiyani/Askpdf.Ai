2713c16a8ddee546d5d3a3c7a1ca642428db9925
Hadiani <80145828+lahadiyani@users.noreply.github.com>
2025-03-23 09:35:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import json, os
-from app.route import app
+import json
+import os
+from app.route import app  # Pastikan app.route ada `app = Flask(__name__)`
 from app.constants.paths import UPLOAD_FOLDER, HISTORY_FILE
 
-if __name__=='__main__':
+if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+    port = int(os.environ.get("PORT", 8080))  # Railway pakai PORT dari env
 
     # Inisialisasi file history jika belum ada
     if not os.path.exists(HISTORY_FILE):
         with open(HISTORY_FILE, "w") as f:
             json.dump([], f)
 
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=port)
